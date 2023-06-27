2be238d54e0a50a5ad79c133ed2ec3dd22a0215e
Drew Jordan <ajordan12@luc.edu>
2023-06-26 20:54:21 -0500

    production tag model update

---
+++ models/productionTag.js
@@ -0,0 +1,29 @@
+// housekeeping 
+const {Model , DataTypes} = require('sequelize');
+const sequelize = require('/config/connection.js');
+const { Product } = require('.');
+
+// Create our Category model
+class Category extends Model {}
+Product.init(
+    {
+        // define columns
+        id: {
+            // use the special Sequelize DataTypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category'
+    }
+);
