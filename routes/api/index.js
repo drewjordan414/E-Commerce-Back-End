7e98a567a2334a9260e49a522af68f67394dfef0
Drew Jordan <ajordan12@luc.edu>
2023-06-26 21:04:32 -0500

    index.js

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+// housekeeping 
+const router = require('express').Router();
+const categoryRoutes = require('./categoryRoutes');
+const productRoutes = require('./productRoutes');
+const tagRoutes = require('./tagRoutes');
+
+// router
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
